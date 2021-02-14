aee657a4451c99419642818398dedf779fce629a
Saurav Singh Rauthan <sauravsinghrauthan@gmail.com>
2021-02-14 12:16:04 +0530

    3rd commit

--- server.js
+++ server.js
@@ -23,11 +23,7 @@ app.use((req,res,next) => {
     next()
 })
 
-app.use((req,res,next) => {
-    res.render('maintainence.hbs')
-})
-
-// app.get('/maintainence',(req,res) => {
+// app.use((req,res,next) => {
 //     res.render('maintainence.hbs')
 // })
 
